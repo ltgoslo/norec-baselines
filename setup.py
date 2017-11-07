b7bb9b44f3dd473390449c859cc6c5b494640751
Eivind Alexander Bergem <eivinabe@ifi.uio.no>
2017-11-07 14:56:30 +0000

    Setup script

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='norec-baselines',
+      version='1.0.0',
+      description='Pre-trained baseline models for NoReC',
+      url='https://github.com/ltgoslo/norec-baselines',
+      author='Eivind Alexander Bergem',
+      author_email='eivinabe@ifi.uio.no',
+      license="gpl",
+      py_modules=['norec_baselines'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Text Processing :: Linguistic',
+          'License :: OSI Approved :: GPL License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'],
+      keywords='nlp',)
